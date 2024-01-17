b5635e821a5cc8c9564f6ebb75532fcea02a7848
Viktor Kertesz <viktor.kertesz@exxonmobil.com>
2024-01-17 12:53:41 +0100

    added highlevel filter logic

--- pyfortinet/fmg_api/common.py
+++ pyfortinet/fmg_api/common.py
@@ -1,5 +1,5 @@
 """Commmon objects"""
-from typing import Literal, Optional
+from typing import Literal, Optional, List, Union
 
 from pydantic import BaseModel, Field
 from pydantic.dataclasses import dataclass
@@ -66,3 +66,154 @@ class BaseDevice(BaseModel):
     os_ver: Optional[OS_VER] = None
     patch: Optional[int] = None
     sn: Optional[str] = Field(None, description="Serial number")
+
+
+"""
+Operator	# of target(s)	Descriptions
+"=="        1               Equal to
+"!="        1               Not equal to
+"<"         1               Less than
+"<="        1               Less than or equal to
+">"         1               Greater than
+">="        1               Greater than or equal to
+"&"	        1               Bitwise AND, target can be integer value only, test if (source & target) = 0
+"&"	        2               Bitwise AND, target can be integer value only, test if (source & target1) = target2
+"in"        1 or more       Test if source is one of the values in target
+"contain"   1               If source have a list of values, test if it contains target
+"like"      1               SQL pattern matching, where target is a string using % (any character) and _ (single 
+                            character) wildcard
+"!like"     1               Not like, inverse of "like" operation
+"glob"      1               Case-sensitive pattern matching with target string using UNIX wildcards
+"!glob"     1               Not glob, inverse of "glob" operation
+"&&"        1               Logical AND operator for nested filter with multiple criteria, where source and target must
+                            be another filter
+"||"        1               Logical OR operator for nested filter with multiple criteria, where source and target must
+                            be another filter
+"""
+
+OP = {
+    "eq": "==",
+    "neq": "!=",
+    "lt": "<",
+    "le": "<=",
+    "gt": ">",
+    "ge": ">=",
+    "or": "&",
+    "in": "in",
+    "contain": "contain",
+    "like": "like",
+    "not_like": "!like",
+    "glob": "glob",
+    "not_glob": "!glob"
+}
+
+
+class F:
+    negate: bool = False
+    source: str = ""
+    op: str = ""
+    targets: Union[List[Union[int, str]], Union[int, str]]
+
+    def __init__(self, **kwargs):
+        """Filter initialization
+
+        We expect calls from
+
+        """
+        if len(kwargs) > 1:
+            raise ValueError(f"F only accepts one filter condition at a time!")
+        for key, value in kwargs.items():
+            if "__" in key:
+                self.source, self.op = key.split("__")
+                if self.op not in OP:
+                    raise ValueError(f"Unknown operation: '{self.op}' !")
+                self.op = OP[self.op]
+            else:
+                self.source = key
+                self.op = "=="
+            self.targets = value
+
+    def generate(self):
+        """Generate API filter list"""
+        out = []
+        if self.negate:
+            out.append("!")
+        out.append(self.source)
+        out.append(self.op)
+        if isinstance(self.targets, list):
+            out.extend(self.targets)
+        else:
+            out.append(self.targets)
+        return out
+
+    def __and__(self, other) -> "ComplexFilter":
+        return ComplexFilter(self, "&&", other)
+        # if isinstance(other, type(self)):
+        #     return [self.generate(), "&&", other.generate()]
+        # elif isinstance(other, list):
+        #     return [self.generate(), "&&", other]
+
+    def __or__(self, other) -> "ComplexFilter":
+        return ComplexFilter(self, "||", other)
+        # if isinstance(other, type(self)):
+        #     return [self.generate(), "||", other.generate()]
+        # elif isinstance(other, list):
+        #     return [self.generate(), "||", other]
+
+    def __invert__(self):
+        self.negate = True
+        return self
+
+    def __add__(self, other: Union["F", "FilterList"]):
+        return FilterList(self, other)
+
+
+class FilterList:
+    members: list[F]
+
+    def __init__(self, *members: Union[F, "FilterList"]):
+        self.members = []
+        for member in members:
+            self + member
+
+    def __add__(self, other: Union[F, "FilterList"]):
+        if isinstance(other, F):
+            self.members.append(other)
+        elif isinstance(other, FilterList):
+            self.members.extend(other.members)
+        else:
+            raise ValueError(f"Elements '{other}' can't be added to FilterList")
+        return self
+
+    def __and__(self, other) -> "ComplexFilter":
+        return ComplexFilter(self, "&&", other)
+
+    def __or__(self, other) -> "ComplexFilter":
+        return ComplexFilter(self, "||", other)
+
+    def generate(self):
+        return [member.generate() for member in self.members]
+
+
+class ComplexFilter:
+    def __init__(self,
+                 a: Union["ComplexFilter", FilterList, F],
+                 op: Literal["||", "&&"],
+                 b: Union["ComplexFilter", FilterList, F]):
+        self.a = a
+        self.op = op
+        self.b = b
+
+    def generate(self):
+        out = [
+            self.a.generate(),
+            self.op,
+            self.b.generate()
+        ]
+        return out
+
+    def __and__(self, other) -> "ComplexFilter":
+        return ComplexFilter(self, "&&", other)
+
+    def __or__(self, other):
+        return ComplexFilter(self, "||", other)
---
+++ tests/test_common.py
@@ -0,0 +1,64 @@
+"""Test offline classes and functions"""
+import pytest
+from pyfortinet.fmg_api.common import F
+
+
+class TestFilters:
+    def test_simple_filter(self):
+        f = F(name="test_address").generate()
+        assert f == ["name", "==", "test_address"]
+
+    def test_negate_filter(self):
+        f = (~F(name="test_address")).generate()
+        assert f == ["!", "name", "==", "test_address"]
+
+    def test_filter_with_more_values(self):
+        f = F(member__in=["abc", "def", "ghi"]).generate()
+        assert f == ["member", "in", "abc", "def", "ghi"]
+
+    def test_filter_with_implicit_or(self):
+        f = (
+                F(name="test_address") + F(name="test2_address")
+        ).generate()
+        assert f == [["name", "==", "test_address"], ["name", "==", "test2_address"]]
+
+    def test_explicit_or_filters(self):
+        f = (F(name="test_address") | F(name="prod_address")).generate()
+        assert f == [["name", "==", "test_address"], "||", ["name", "==", "prod_address"]]
+
+    def test_multiple_filters(self):
+        f = (
+                F(name="acceptance_address") | F(name="test_address") | F(name="prod_address")
+        ).generate()
+        assert f == [
+            [["name", "==", "acceptance_address"], "||", ["name", "==", "test_address"]],
+            "||", ["name", "==", "prod_address"]
+        ]
+
+    def test_filters_with_parentheses(self):
+        f = (
+                F(name="acceptance_address") | (F(name="test_address") | F(name="prod_address"))
+        ).generate()
+        assert f == [["name", "==", "acceptance_address"], "||",
+                     [["name", "==", "test_address"], "||", ["name", "==", "prod_address"]]]
+
+    def test_filters_with_parentheses2(self):
+        f = (
+                (F(name="acceptance_address") | F(name="test_address")) & F(state=1)
+        ).generate()
+        assert f == [[["name", "==", "acceptance_address"], "||", ["name", "==", "test_address"]],
+                     "&&", ["state", "==", 1]]
+
+    def test_and_filters(self):
+        f = (F(name__like="test%") & F(interface="port1")).generate()
+        assert f == [["name", "like", "test%"], "&&", ["interface", "==", "port1"]]
+
+    def test_complex_filter(self):
+        f = (
+            (F(name="root") + F(name="rootp")) &
+            (F(status=1) + F(status=2))
+        ).generate()
+        assert f == [
+            [["name", "==", "root"], ["name", "==", "rootp"]], "&&",
+            [["status", "==", 1], ["status", "==", 2]]
+        ]
