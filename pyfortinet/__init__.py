aaa477b766f76b79a1ea38d93843bd3bb4703f82
Viktor Kertesz <vkertesz2@gmail.com>
2024-01-21 23:25:34 +0100

    add FMGResponse to global namespace
    This ease extending FMGBase at external projects

--- pyfortinet/__init__.py
+++ pyfortinet/__init__.py
@@ -1,8 +1,8 @@
 """Fortinet Python library"""
 __version__ = "0.0.1"
 from pyfortinet.async_fmg import AsyncFMGBase
-from pyfortinet.fmgbase import FMGBase
+from pyfortinet.fmgbase import FMGBase, FMGResponse
 from pyfortinet.fmg import FMG
 from pyfortinet.settings import FMGSettings
 
-__all__ = ("FMGBase", "FMG", "AsyncFMGBase", "FMGSettings")
+__all__ = ("FMGBase", "FMG", "FMGResponse", "AsyncFMGBase", "FMGSettings")
