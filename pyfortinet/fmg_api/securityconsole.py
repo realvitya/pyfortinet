328855e072d7ac3e4e21e03757fd6f8fc19adf95
Viktor Kertesz <vkertesz2@gmail.com>
2024-03-25 00:25:21 +0100

    docs update

--- pyfortinet/fmg_api/securityconsole.py
+++ pyfortinet/fmg_api/securityconsole.py
@@ -8,6 +8,14 @@ INSTALL_DEVICE_FLAGS = Literal["none", "preview", "auto_lock_ws"]
 
 
 class InstallDeviceTask(FMGExecObject):
+    """Install device settings
+
+    Attributes:
+        adom (str): ADOM to use
+        dev_rev_comments (str): Device revision comments
+        flags (List[INSTALL_DEVICE_FLAGS]): flags for the task
+        scope (List[Scope]): scopes for the task (e.g. group name or device with vdom)
+    """
     _url = "/securityconsole/install/device"
     adom: Optional[str] = None
     dev_rev_comments: Optional[str] = None
