968ccb6097bfe0c2b0fbc5fa5797e0c44746b187
Viktor Kertesz <vkertesz2@gmail.com>
2024-03-27 01:04:07 +0100

    Address object enhanced.
    Dynamic mapping works.

--- pyfortinet/fmg_api/firewall.py
+++ pyfortinet/fmg_api/firewall.py
@@ -1,13 +1,14 @@
 """Firewall object types"""
 
-from ipaddress import IPv4Interface
-from typing import Literal, Optional, Union
+from ipaddress import IPv4Interface, IPv4Address
+from typing import Literal, Optional, Union, List
 from uuid import UUID
 
 from more_itertools import first
-from pydantic import Field, ValidationInfo, field_validator, AliasChoices
+from pydantic import Field, ValidationInfo, field_validator, AliasChoices, BaseModel
 
 from pyfortinet.fmg_api import FMGObject
+from pyfortinet.fmg_api.common import Scope
 
 ADDRESS_GROUP_TYPE = Literal["default", "array", "folder"]
 ADDRESS_GROUP_CATEGORY = Literal["default", "ztna-ems-tag", "ztna-geo-tag"]
@@ -27,6 +28,13 @@ ADDRESS_TYPE = Literal[
     "mac",
     "fqdn-group",
 ]
+CLEARPASS_SPT = Literal["unknown", "healthy", "quarantine", "checkup", "transition", "infected", "transient"]
+DIRTY = Literal["dirty", "clean"]
+FABRIC_OBJECT = Literal["disable", "enable"]
+NODE_IP_ONLY = Literal["disable", "enable"]
+OBJ_TYPE = Literal["ip", "mac"]
+SDN_ADDR_TYPE = Literal["private", "public", "all"]
+SUB_TYPE = Literal[ "sdn", "clearpass-spt", "fsso", "ems-tag", "swc-tag", "fortivoice-tag", "fortinac-tag", "fortipolicy-tag" ]
 
 
 class Address(FMGObject):
@@ -34,12 +42,67 @@ class Address(FMGObject):
 
     Attributes:
         name (str): object name
+        allow_routing (str): Defines whether the use of this address in the static route configuration
+                             is enabled or disabled, with possible values being 'disable' or 'enable'.
         associated_interface (str|list[str]): object assigned to interface/zone name
         subnet (str|list[str]): subnet in x.x.x.x/x or [x.x.x.x, y.y.y.y] format
+        cache_ttl (int): Defines the minimal TTL (Time To Live) of individual IP addresses in FQDN
+                         cache measured in seconds.
+        clearpass_spt (str): Represents the SPT (System Posture Token) value, indicating system status.
+                             Possible values include 'healthy', 'quarantine', 'transition', etc.
+        color (int): color code for the address object icon on the GUI.
+        comment (str): comment for the address object.
+        country (str): IP addresses associated to a specific country.
+        dirty (str): Indicates whether the address is to be deleted; possible values 'dirty' or 'clean'.
+        end_ip (str): The final IP address (inclusive) in the range for the address.
+        epg_name (str): endpoint group name.
+        fabric_object (str): Indicates the Security Fabric global object setting,
+                             with possible values being 'disable' or 'enable'.
+        filter (str): Match criteria filter.
+        fqdn (str): Fully Qualified Domain Name address.
+        fsso_group (List[str]): A list of FSSO group(s).
+        interface (str): Name of interface whose IP address is to be used.
+        list (List[AddressList]): List (TODO: figure out, docs don't help)
+        macaddr (List[str]): Multiple MAC address ranges.
+        node_ip_only (str): Defines whether only the collection of node addresses in Kubernetes
+                            is enabled or disabled. Possible values are 'disable' or 'enable'.
+        obj_id (str): Object ID for NSX.
+        obj_tag (str): Tag of dynamic address object.
+        obj_type (str): type of the object (IP, MAC)
+        organization (str): Organization domain name (Syntax: organization/domain).
+        policy_group (str): policy group name.
+        sdn (str): SDN.
+        sdn_addr_type (str): Type of addresses to collect.
+        sdn_tag (str): SDN tag.
+        start_ip (str): First IP address (inclusive) in the range for the address.
+        sub_type (str): Indicates the sub-type of address.
+                        Possible values include 'sdn', 'clearpass-spt', 'fsso', etc.
+        subnet_name (str): Subnet name.
+        tag_detection_level (str): Tag detection level of dynamic address object.
+        tag_type (str): Tag type of dynamic address object.
+        tagging (List[AddressTagging]): tagging details for this address.
+        tenant (str): tenant related to this address.
+        type (str): Indicates the type of address. Possible values include 'ipmask', 'iprange', 'fqdn', etc.
+        uuid (str): Contains the Universally Unique Identifier (UUID; automatically assigned but can be manually reset).
+        wildcard (str): This is the IP address and wildcard netmask.
+        wildcard_fqdn (str): Contains a Fully Qualified Domain Name with wildcard characters.
+        global_object (int): global object related to this address.
+        mapping__scope (List[dict, Scope]): the mapping scope for this address.
     """
+    class AddressList(BaseModel):
+        ip: Optional[str] = None
+        net_id: Optional[str] = Field(None, validation_alias=AliasChoices("net-id", "net_id"),
+                                      serialization_alias="net-id")
+        obj_id: Optional[str] = Field(None, validation_alias=AliasChoices("obj-id", "obj_id"),
+                                      serialization_alias="obj-id")
+
+    class AddressTagging(BaseModel):
+        category: Optional[str] = None
+        name: Optional[str] = None
+        tags: Optional[List[str]] = None
 
     _url: str = "/pm/config/{scope}/obj/firewall/address"
-    name: str = Field(..., max_length=128)
+    name: Optional[str] = Field(None, max_length=128)
     allow_routing: Optional[ALLOW_ROUTING] = Field(
         None, validation_alias=AliasChoices("allow-routing", "allow_routing"), serialization_alias="allow-routing"
     )
@@ -48,10 +111,48 @@ class Address(FMGObject):
         validation_alias=AliasChoices("associated-interface", "associated_interface"),
         serialization_alias="associated-interface",
     )
-    subnet: Optional[Union[str, list[str]]] = None
+    cache_ttl: Optional[int] = Field(None, validation_alias=AliasChoices("cache-ttl", "cache_ttl"), serialization_alias="cache-ttl")
+    clearpass_spt: Optional[CLEARPASS_SPT] = Field(None, validation_alias=AliasChoices("clearpass-spt", "clearpass_spt"), serialization_alias="clearpass-spt")
+    color: Optional[int] = None
+    comment: Optional[str] = None
+    country: Optional[str] = None
+    dirty: Optional[DIRTY] = None
+    dynamic_mapping: Optional[list["Address"]] = None
+    end_ip: Optional[str] = None
+    epg_name: Optional[str] = Field(None, validation_alias=AliasChoices("epg-name", "epg_name"), serialization_alias="epg-name")
+    fabric_object: Optional[FABRIC_OBJECT] = Field(None, validation_alias=AliasChoices("fabric-object", "fabric_object"), serialization_alias="fabric-object")
+    filter: Optional[str] = None
+    fqdn: Optional[str] = None
+    fsso_group: Optional[List[str]] = Field(None, validation_alias=AliasChoices("fsso-group", "fsso_group"), serialization_alias="fsso-group")
+    interface: Optional[str] = None
+    list: Optional[List["AddressList"]] = None
+    macaddr: Optional[List[str]] = None
+    node_ip_only: Optional[NODE_IP_ONLY] = Field(None, validation_alias=AliasChoices("node-ip-only", "node_ip_only"), serialization_alias="node-ip-only")
+    obj_id: Optional[str] = Field(None, validation_alias=AliasChoices("obj-id", "obj_id"), serialization_alias="obj-id")
+    obj_tag: Optional[str] = Field(None, validation_alias=AliasChoices("obj-tag", "obj_tag"), serialization_alias="obj-tag")
+    obj_type: Optional[OBJ_TYPE] = Field(None, validation_alias=AliasChoices("obj-type", "obj_type"), serialization_alias="obj-type")
+    organization: Optional[str] = None
+    policy_group: Optional[str] = Field(None, validation_alias=AliasChoices("policy-group", "policy_group"), serialization_alias="policy-group")
+    sdn: Optional[str] = None
+    sdn_addr_type: Optional[SDN_ADDR_TYPE] = Field(None, validation_alias=AliasChoices("sdn-addr-type", "sdn_addr_type"), serialization_alias="sdn-addr-type")
+    sdn_tag: Optional[str] = Field(None, validation_alias=AliasChoices("sdn-tag", "sdn_tag"), serialization_alias="sdn-tag")
+    start_ip: Optional[str] = Field(None, validation_alias=AliasChoices("start-ip", "start_ip"), serialization_alias="start-ip")
+    sub_type: Optional[SUB_TYPE] = Field(None, validation_alias=AliasChoices("sub-type", "sub_type"), serialization_alias="sub-type")
+    subnet: Optional[Union[str, List[str]]] = None
+    subnet_name: Optional[str] = Field(None, validation_alias=AliasChoices("subnet-name", "subnet_name"), serialization_alias="subnet-name")
+    tag_detection_level: Optional[str] = Field(None, validation_alias=AliasChoices("tag-detection-level", "tag_detection_level"), serialization_alias="tag-detection-level")
+    tag_type: Optional[str] = Field(None, validation_alias=AliasChoices("tag-type", "tag_type"), serialization_alias="tag-type")
+    tagging: Optional[List[AddressTagging]] = None
+    tenant: Optional[str] = None
     type: Optional[ADDRESS_TYPE] = None
-    url: Optional[str] = None
     uuid: Optional[str] = None
+    wildcard: Optional[str] = None
+    wildcard_fqdn: Optional[str] = Field(None, validation_alias=AliasChoices("wildcard-fqdn", "wildcard_fqdn"), serialization_alias="wildcard-fqdn")
+    # Mapping fields
+    global_object: Optional[int] = Field(
+        None, validation_alias=AliasChoices("global-object", "global_object"), serialization_alias="global-object"
+    )
+    mapping__scope: Optional[List[Union[dict, Scope]]] = Field(None, validation_alias=AliasChoices("_scope", "mapping__scope"), serialization_alias="_scope")
 
     @field_validator("subnet")
     def standardize_subnet(cls, v):
@@ -74,19 +175,60 @@ class Address(FMGObject):
             return v
 
     @field_validator("allow_routing", mode="before")
-    def validate_allow_routing(cls, v: int) -> ALLOW_ROUTING:
+    def validate_allow_routing(cls, v) -> ALLOW_ROUTING:
         return ALLOW_ROUTING.__dict__.get("__args__")[v] if isinstance(v, int) else v
 
     @field_validator("type", mode="before")
-    def validate_type(cls, v: int) -> ADDRESS_TYPE:
+    def validate_type(cls, v) -> ADDRESS_TYPE:
         return ADDRESS_TYPE.__dict__.get("__args__")[v] if isinstance(v, int) else v
 
+    @field_validator("clearpass_spt", mode="before")
+    def validate_clearpass_spt(cls, v) -> CLEARPASS_SPT:
+        return CLEARPASS_SPT.__dict__.get("__args__")[v] if isinstance(v, int) else v
 
-class AddressMapping(Address):
-    _url: str = "/pm/config/{scope}/obj/firewall/address/{address}/dynamic_mapping"
-    global_object: int = Field(
-        ..., validation_alias=AliasChoices("global-object", "global_object"), serialization_alias="global-object"
-    )
+    # @field_validator("dirty", mode="before")
+    # def validate_dirty(cls, v: int) -> DIRTY:
+    #     return DIRTY.__dict__.get("__args__")[v] if isinstance(v, int) else v
+    # got value 2=dirty!
+
+    @field_validator("end_ip", mode="before")
+    def validate_end_ip(cls, v: str) -> str:
+        assert IPv4Address(v)
+        return v
+
+    @field_validator("fabric_object", mode="before")
+    def validate_fabric_object(cls, v) -> str:
+        return FABRIC_OBJECT.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("node_ip_only", mode="before")
+    def validate_node_ip_only(cls, v) -> str:
+        return NODE_IP_ONLY.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    # @field_validator("obj_type", mode="before")
+    # def validate_obj_type(cls, v: int) -> str:
+    #     return OBJ_TYPE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+    # got value 7=ip!
+
+    @field_validator("sdn_addr_type", mode="before")
+    def validate_sdn_addr_type(cls, v) -> str:
+        return SDN_ADDR_TYPE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("start_ip", mode="before")
+    def validate_start_ip(cls, v: str) -> str:
+        assert IPv4Address(v)
+        return v
+
+    @field_validator("sub_type", mode="before")
+    def validate_sub_type(cls, v) -> str:
+        return SUB_TYPE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("wildcard", mode="before")
+    def validate_wildcard(cls, v) -> str:
+        """Ensure wildcard address definition is correct (x.x.x.x y.y.y.y)"""
+        if isinstance(v, list):
+            return " ".join(IPv4Address(part).compressed for part in v)
+        else:  # string
+            return " ".join(IPv4Address(part).compressed for part in v.split())
 
 
 class AddressGroup(FMGObject):
@@ -101,16 +243,3 @@ class AddressGroup(FMGObject):
     type: ADDRESS_GROUP_TYPE = "default"
     uuid: Optional[UUID]
     dynamic_mapping: list["AddressGroupMapping"]
-
-
-class AddressGroupMapping(AddressGroup):
-    _url: str = "/pm/config/{scope}/obj/firewall/addrgrp/{addrgrp}/dynamic_mapping"
-    global_object: int = Field(
-        ..., validation_alias=AliasChoices("global-object", "global_object"), serialization_alias="global-object"
-    )
-
-    @field_validator("_url", check_fields=False)
-    def construct_url(cls, v: str, info: ValidationInfo):
-        """rewrite URL with actual address group name"""
-        url = v.replace("{addrgrp}", info.data["name"])
-        return super().construct_url(url, info)
--- tests/fmg/objects/test_firewall.py
+++ tests/fmg/objects/test_firewall.py
@@ -1,7 +1,6 @@
 """Test of human API"""
 
 import pytest
-from pyfortinet import FMG, FMGSettings
 from pyfortinet.fmg_api.common import F
 from pyfortinet.fmg_api.dvmdb import Device
 from pyfortinet.fmg_api.firewall import Address
@@ -10,10 +9,17 @@ from pyfortinet.fmg_api.firewall import Address
 @pytest.mark.usefixtures("fmg")
 class TestObjectsOnLab:
     def test_firewall_address(self, fmg):
-        to_add = fmg.get_obj(Address, name="test-firewall-address", subnet="10.0.0.0/24", allow_routing="disable")
+        to_add = fmg.get_obj(Address(name="test-firewall-address", subnet="10.0.0.0/24", allow_routing="disable"))
         # test ADD
         result = to_add.add()
         assert result
+        # wildcard test
+        wildcard = fmg.get_obj(Address(name="test-wildcard", type="wildcard", wildcard="10.0.0.1 255.255.0.255"))
+        result = wildcard.add()
+        assert result
+        wildcard = fmg.get(Address, F(name="test-wildcard")).first()
+        assert wildcard
+        wildcard.delete()
         # test GET
         address = fmg.get(Address, F(name__like="test-firewall-addr%")).first()
         assert address.name == "test-firewall-address"
@@ -36,7 +42,36 @@ class TestObjectsOnLab:
         result = fmg.get(Address, F(name="test-firewall-address"))
         assert result and not result.data  # ensure empty result
 
-    # def test_firewall_address_mapping(self, fmg):
-    #     server = fmg.get_obj(Address, name="test-server", subnet="10.0.0.1/32")
-    #     FG01 = fmg.get(Device, F(name="FG01")).first()
-    #     assert True
+    def test_firewall_address_mapping(self, fmg):
+        # create a new object
+        server: Address = fmg.get_obj(Address(name="test-server", subnet="10.0.0.1/32"))
+        # get first device from FMG
+        fw: Device = fmg.get(Device).first()
+        # create server object in FMG
+        server.add()
+        # create a mapping to server object with the fw device and different IP
+        server.dynamic_mapping = [Address(mapping__scope=[{"name": fw.name, "vdom": "root"}], subnet="2.2.2.2")]
+        # update server object in FMG
+        result = server.update()
+        assert result
+        # re-load server object from FMG
+        server = fmg.get(Address, F(name="test-server")).first()
+        # check if we have our mapping
+        assert any(address.subnet == "2.2.2.2/32" for address in server.dynamic_mapping)
+        #
+        # commented out as I referenced a 2nd FW (FG02) which I do not expect in the lab
+        #
+        # # create new mapping with FG02 device (I created a model device for testing)
+        # server.dynamic_mapping.append(Address(mapping__scope=[{"name": "FG02", "vdom": "root"}], subnet="3.3.3.3"))
+        # result = server.update()
+        # assert result
+        # server = fmg.get(Address, F(name="test-server")).first()
+        # assert server
+        # # delete mapping (simply removing from the list works. Could be more sophisticated, but good for example)
+        # del server.dynamic_mapping[1]
+        # server.update()
+        # server = fmg.get(Address, F(name="test-server")).first()
+        # # check if we still have the first mapping
+        # assert any(address.subnet == "2.2.2.2/32" for address in server.dynamic_mapping)
+        # # check if we really deleted the second mapping
+        # assert not any(address.subnet == "3.3.3.3/32" for address in server.dynamic_mapping)
