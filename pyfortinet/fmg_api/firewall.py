2472b362ad941d55ec801700256a967f2386a2b1
Viktor Kertesz <vkertesz2@gmail.com>
2024-05-14 12:53:29 +0200

    Service related classes added

--- pyfortinet/fmg_api/firewall.py
+++ pyfortinet/fmg_api/firewall.py
@@ -35,6 +35,34 @@ SDN_ADDR_TYPE = Literal["private", "public", "all"]
 SUB_TYPE = Literal[
     "sdn", "clearpass-spt", "fsso", "ems-tag", "swc-tag", "fortivoice-tag", "fortinac-tag", "fortipolicy-tag"
 ]
+APP_SERVICE_TYPE = Literal["disable", "app-id", "app-category"]
+CHECK_RESET_RANGE = Literal["disable", "default", "strict"]
+HELPER = Literal[
+    "disable",
+    "auto",
+    "ftp",
+    "tftp",
+    "ras",
+    "h323",
+    "tns",
+    "mms",
+    "sip",
+    "pptp",
+    "rtsp",
+    "dns-udp",
+    "dns-tcp",
+    "pmap",
+    "rsh",
+    "dcerpc",
+    "mgcp",
+    "gtp-c",
+    "gtp-u",
+    "gtp-b",
+    "pfcp",
+]
+PROTOCOL = Literal[
+    "ICMP", "IP", "TCP/UDP/SCTP", "ICMP6", "HTTP", "FTP", "CONNECT", "SOCKS", "ALL", "SOCKS-TCP", "SOCKS-UDP"
+]
 
 
 class AddressTagging(BaseModel):
@@ -291,12 +319,17 @@ class AddressGroup(FMGObject):
     dynamic_mapping: Optional[Union[List["AddressGroup"], "AddressGroup"]] = None
     exclude: Optional[ENABLE_DISABLE] = None
     exclude_member: Optional[List[Union[str, Address, "AddressGroup"]]] = Field(
-        None, max_length=5000, validation_alias=AliasChoices("exclude-member", "exclude_member"), serialization_alias="exclude-member"
+        None,
+        max_length=5000,
+        validation_alias=AliasChoices("exclude-member", "exclude_member"),
+        serialization_alias="exclude-member",
     )
-    fabric_object: Optional[ENABLE_DISABLE] = None
-    member: Optional[List[Union[str, Address, "AddressGroup"]]] = Field(
-        None, max_length=5000
+    fabric_object: Optional[ENABLE_DISABLE] = Field(
+        None,
+        validation_alias=AliasChoices("fabric-object", "fabric_object"),
+        serialization_alias="fabric-object",
     )
+    member: Optional[List[Union[str, Address, "AddressGroup"]]] = Field(None, max_length=5000)
     name: Optional[str] = None
     tagging: Optional[List[AddressTagging]] = None
     type: Optional[ADDRESS_GROUP_TYPE] = "default"
@@ -313,20 +346,12 @@ class AddressGroup(FMGObject):
             serialized.append(member.name)
         return serialized
 
-    @field_validator("allow_routing", mode="before")
-    def validate_allow_routing(cls, v) -> ENABLE_DISABLE:
-        return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
-
     @field_validator("category", mode="before")
     def validate_category(cls, v) -> ADDRESS_GROUP_CATEGORY:
         return ADDRESS_GROUP_CATEGORY.__dict__.get("__args__")[v] if isinstance(v, int) else v
 
-    @field_validator("exclude", mode="before")
-    def validate_exclude(cls, v) -> ENABLE_DISABLE:
-        return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
-
-    @field_validator("fabric_object", mode="before")
-    def validate_fabric_object(cls, v) ->ENABLE_DISABLE:
+    @field_validator("allow_routing", "exclude", "fabric_object", mode="before")
+    def validate_enable_disable(cls, v) -> ENABLE_DISABLE:
         return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
 
     @field_validator("type", mode="before")
@@ -337,3 +362,300 @@ class AddressGroup(FMGObject):
     def validate_uuid(cls, v) -> str:
         assert UUID(v)
         return str(v)
+
+
+class ServiceCategory(FMGObject):
+    _url = "/pm/config/{scope}/firewall/service/category"
+    _master_keys = ["name"]
+    comment: Optional[str] = None
+    fabric_object: Optional[ENABLE_DISABLE] = Field(
+        None,
+        validation_alias=AliasChoices("fabric-object", "fabric_object"),
+        serialization_alias="fabric-object",
+    )
+    name: Optional[str] = None
+
+    @field_validator("fabric_object", mode="before")
+    def standardize_enabled_disabled(cls, v):
+        return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+
+class ServiceCustom(FMGObject):
+    """Custom service class
+
+    Attributes:
+        app_category
+        app_service_type
+        application
+        category
+        check_reset_range
+        color
+        comment
+        fabric_object
+        fqdn
+        helper
+        icmpcode
+        icmptype
+        iprange
+        name
+        protocol
+        protocol_number
+        proxy
+        sctp_portrange
+        session_ttl
+        tcp_halfclose_timer
+        tcp_halfopen_timer
+        tcp_portrange
+        tcp_rst_timer
+        tcp_timewait_timer
+        udp_idle_timer
+        udp_portrange
+        visibility
+    """
+    _url = "/pm/config/{scope}/firewall/service/custom"
+    _master_keys = ["name"]
+    app_category: Optional[List[int]] = Field(
+        None,
+        validation_alias=AliasChoices("app-category", "app_category"),
+        serialization_alias="app-category",
+    )
+    app_service_type: Optional[APP_SERVICE_TYPE] = Field(
+        None,
+        validation_alias=AliasChoices("app-service-type", "app_service_type"),
+        serialization_alias="app-service-type",
+    )
+    application: Optional[int] = None
+    category: Optional[ServiceCategory] = None
+    check_reset_range: Optional[CHECK_RESET_RANGE] = Field(
+        None,
+        validation_alias=AliasChoices("check-reset-range", "check_reset_range"),
+        serialization_alias="check-reset-range",
+    )
+    color: Optional[int] = None
+    comment: Optional[str] = None
+    fabric_object: Optional[ENABLE_DISABLE] = Field(
+        None,
+        validation_alias=AliasChoices("fabric-object", "fabric_object"),
+        serialization_alias="fabric-object",
+    )
+    fqdn: Optional[str] = None
+    helper: Optional[HELPER] = None
+    icmpcode: Optional[int] = None
+    icmptype: Optional[int] = None
+    iprange: Optional[str] = None
+    name: Optional[str] = None
+    protocol: Optional[PROTOCOL] = None
+    protocol_number: Optional[int] = Field(
+        None,
+        validation_alias=AliasChoices("protocol-number", "protocol_number"),
+        serialization_alias="protocol-number",
+    )
+    proxy: Optional[ENABLE_DISABLE] = None
+    sctp_portrange: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("sctp-portrange", "sctp_portrange"),
+        serialization_alias="sctp-portrange",
+    )
+    session_ttl: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("session-ttl", "session_ttl"),
+        serialization_alias="session-ttl",
+    )
+    tcp_halfclose_timer: Optional[int] = Field(
+        None,
+        validation_alias=AliasChoices("tcp-halfclose-timer", "tcp_halfclose_timer"),
+        serialization_alias="tcp-halfclose-timer",
+    )
+    tcp_halfopen_timer: Optional[int] = Field(
+        None,
+        validation_alias=AliasChoices("tcp-halfopen-timer", "tcp_halfopen_timer"),
+        serialization_alias="tcp-halfopen-timer",
+    )
+    tcp_portrange: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("tcp-portrange", "tcp_portrange"),
+        serialization_alias="tcp-portrange",
+    )
+    tcp_rst_timer: Optional[int] = Field(
+        None, validation_alias=AliasChoices("tcp-rst-timer", "tcp_rst_timer"), serialization_alias="tcp-rst-timer"
+    )
+    tcp_timewait_timer: Optional[int] = Field(
+        None,
+        validation_alias=AliasChoices("tcp-timewait-timer", "tcp_timewait_timer"),
+        serialization_alias="tcp-timewait-timer",
+    )
+    udp_idle_timer: Optional[int] = Field(
+        None, validation_alias=AliasChoices("udp-idle-timer", "udp_idle_timer"), serialization_alias="udp-idle-timer"
+    )
+    udp_portrange: Optional[str] = Field(
+        None, validation_alias=AliasChoices("udp-portrange", "udp_portrange"), serialization_alias="udp-portrange"
+    )
+    visibility: Optional[ENABLE_DISABLE] = None
+
+    @field_validator("app_service_type", mode="before")
+    def standardize_app_service_type(cls, v):
+        return APP_SERVICE_TYPE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("check_reset_range", mode="before")
+    def standardize_check_reset_range(cls, v):
+        return CHECK_RESET_RANGE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("fabric_object", "proxy", "visibility", mode="before")
+    def standardize_enabled_disabled(cls, v):
+        return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("helper", mode="before")
+    def standardize_helper(cls, v):
+        return HELPER.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+    @field_validator("protocol", mode="before")
+    def standardize_protocol(cls, v):
+        return PROTOCOL.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+
+class ServiceGroup(FMGObject):
+    """Service group class
+
+    Attributes:
+
+    """
+    _url = "/pm/config/{scope}/firewall/service/group"
+    _master_keys = ["name"]
+    color: Optional[int] = None
+    comment: Optional[str] = None
+    fabric_object: Optional[ENABLE_DISABLE] = Field(
+        None,
+        validation_alias=AliasChoices("fabric-object", "fabric_object"),
+        serialization_alias="fabric-object",
+    )
+    member: Optional[List[Union[str, ServiceCustom]]] = Field(None, max_length=5000)
+    name: Optional[str] = None
+    proxy: Optional[ENABLE_DISABLE] = None
+
+    @field_serializer("member")
+    def member_names_only(members: List[Union[str, Address, "AddressGroup"]]) -> List[str]:
+        """Ensure member names are passed to API as it is expected"""
+        serialized = []
+        for member in members:
+            if isinstance(member, str):
+                serialized.append(member)
+                continue
+            serialized.append(member.name)
+        return serialized
+
+    @field_validator("fabric_object", "proxy", mode="before")
+    def standardize_enabled_disabled(cls, v):
+        return ENABLE_DISABLE.__dict__.get("__args__")[v] if isinstance(v, int) else v
+
+
+class ProfileGroup(FMGObject):
+    """Service group class
+
+    Attributes:
+        application_list: Name of an existing Application list.
+        av_profile: Name of an existing Antivirus profile.
+        dlp_profile: Name of an existing DLP profile.
+        dnsfilter_profile: Name of an existing DNS filter profile.
+        emailfilter_profile: Name of an existing email filter profile.
+        file_filter_profile: Name of an existing file_filter profile.
+        icap_profile: Name of an existing ICAP profile.
+        ips_sensor: Name of an existing IPS sensor.
+        ips_voip_filter: Name of an existing VoIP (ips) profile.
+        name: Profile group name.
+        profile_protocol_options: Name of an existing Protocol options profile.
+        sctp_filter_profile: Name of an existing SCTP filter profile.
+        ssh_filter_profile: Name of an existing SSH filter profile.
+        ssl_ssh_profile: Name of an existing SSL SSH profile.
+        videofilter_profile: Name of an existing VideoFilter profile.
+        voip_profile: Name of an existing VoIP (voipd) profile.
+        waf_profile: Name of an existing Web application firewall profile.
+        webfilter_profile: Name of an existing Web filter profile.
+    """
+    _url = "/pm/config/{scope}/obj/firewall/profile-group"
+    _master_keys = ["name"]
+    application_list: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("application-list", "application_list"),
+        serialization_alias="application-list"
+    )
+    av_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("av-profile", "av_profile"),
+        serialization_alias="av-profile"
+    )
+    dlp_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("dlp-profile", "dlp_profile"),
+        serialization_alias="dlp-profile"
+    )
+    dnsfilter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("dnsfilter-profile", "dnsfilter_profile"),
+        serialization_alias="dnsfilter-profile"
+    )
+    emailfilter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("emailfilter-profile", "emailfilter_profile"),
+        serialization_alias="emailfilter-profile"
+    )
+    file_filter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("file-filter-profile", "file_filter_profile"),
+        serialization_alias="file-filter-profile"
+    )
+    icap_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("icap-profile", "icap_profile"),
+        serialization_alias="icap-profile"
+    )
+    ips_sensor: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("ips-sensor", "ips_sensor"),
+        serialization_alias="ips-sensor"
+    )
+    ips_voip_filter: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("ips-voip-filter", "ips_voip_filter"),
+        serialization_alias="ips-voip-filter"
+    )
+    name: Optional[str] = None
+    profile_protocol_options: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("profile-protocol-options", "profile_protocol_options"),
+        serialization_alias="profile-protocol-options"
+    )
+    sctp_filter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("sctp-filter-profile", "sctp_filter_profile"),
+        serialization_alias="sctp-filter-profile"
+    )
+    ssh_filter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("ssh-filter-profile", "ssh_filter_profile"),
+        serialization_alias="ssh-filter-profile"
+    )
+    ssl_ssh_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("ssl-ssh-profile", "ssl_ssh_profile"),
+        serialization_alias="ssl-ssh-profile"
+    )
+    videofilter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("videofilter-profile", "videofilter_profile"),
+        serialization_alias="videofilter-profile"
+    )
+    voip_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("voip-profile", "voip_profile"),
+        serialization_alias="voip-profile"
+    )
+    waf_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("waf-profile", "waf_profile"),
+        serialization_alias="waf-profile"
+    )
+    webfilter_profile: Optional[str] = Field(
+        None,
+        validation_alias=AliasChoices("webfilter-profile", "webfilter_profile"),
+        serialization_alias="webfilter-profile"
+    )
